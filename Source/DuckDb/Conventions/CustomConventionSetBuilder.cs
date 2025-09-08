e488e84635d9ea6a0929455162d90c9e316901d5
VikasAtEnEx <vikas.pachori@energyexemplar.com>
2025-09-08 13:14:20 +0530

    addition and conventions and yml fixes (#18)
    * Update CI workflow to allow manual triggers and adjust versioning logic for initial tag
    
    * Add custom conventions for entity and property naming in DuckDb
    
    * Add optional EF Core conventions for automatic entity and property name mapping

---
+++ Source/DuckDb/Conventions/CustomConventionSetBuilder.cs
@@ -0,0 +1,25 @@
+﻿// OPTIONAL: Custom convention set builder that applies EntityNameAsTableNameConvention and PropertyNameAsColumnNameConvention.
+// Usage example:
+// DbContextOptionsBuilder<PlexosInputDbContext> contextBuilder = new DbContextOptionsBuilder<PlexosInputDbContext>();
+// contextBuilder.ReplaceService<IProviderConventionSetBuilder, CustomConventionSetBuilder>();
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EnergyExemplar.EntityFrameworkCore.DuckDb.Conventions
+{
+    public class CustomConventionSetBuilder : SqlServerConventionSetBuilder
+    {
+        public CustomConventionSetBuilder(ProviderConventionSetBuilderDependencies dependencies, RelationalConventionSetBuilderDependencies relationalDependencies, ISqlGenerationHelper sqlGenerationHelper) : base(dependencies, relationalDependencies, sqlGenerationHelper)
+        {
+        }
+        public override ConventionSet CreateConventionSet()
+        {
+            var set = base.CreateConventionSet();
+            set.EntityTypeAddedConventions.Add(new EntityNameAsTableNameConvention());
+            set.PropertyAddedConventions.Add(new PropertyNameAsColumnNameConvention());
+            return set;
+
+        }
+    }
+}
